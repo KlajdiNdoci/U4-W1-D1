c6dd4f584507d7736d9bc669a06b5b89db7479f8
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-10-02 12:58:27 +0200

    Initial commit

---
+++ Example.java
@@ -0,0 +1,8 @@
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("ciao");
+        double ciao = 0.5;
+        String testo = "ciao";
+        System.out.println("la stringa é :" + testo);
+    }
+}
